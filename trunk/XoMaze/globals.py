92816c3970d36e83d9df68ec90f0a5917c8d7733
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 20:15:10 +0000

    all players have keys

--- trunk/XoMaze/globals.py
+++ trunk/XoMaze/globals.py
@@ -3,6 +3,35 @@ import pygame
 # custom events
 CLOCKTICK = pygame.USEREVENT + 1
 
+EMULATORKEYS = {
+	"K_KP8" : [ 0, 0, ],
+	"K_KP6" : [ 0, 1, ],
+	"K_KP2" : [ 0, 2, ],
+	"K_KP4" : [ 0, 3, ],
+}
+
+KEYBOARDKEYS = {
+	"K_w" : [ 0, 0, ],
+	"K_d" : [ 0, 1, ],
+	"K_s" : [ 0, 2, ],
+	"K_a" : [ 0, 3, ],
+	
+	"K_g" : [ 1, 0, ], # up
+	"K_h" : [ 1, 0, ], # up
+	"K_n" : [ 1, 1, ],
+	"K_b" : [ 1, 2, ],
+	"K_v" : [ 1, 3, ],
+	
+	"K_UP" : [ 2, 0, ],
+	"K_RIGHT" : [ 2, 1, ],
+	"K_DOWN" : [ 2, 2, ],
+	"K_LEFT" : [ 2, 3, ],
+	
+	"K_KP8" : [ 3, 0, ],
+	"K_KP6" : [ 3, 1, ],
+	"K_KP2" : [ 3, 2, ],
+	"K_KP4" : [ 3, 3, ],
+}
 # Player variables
 PLAYERXINCREMENT = 0.5
 PLAYERYINCREMENT = 0.5
