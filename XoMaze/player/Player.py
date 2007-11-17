311cd64b067653acdf349a333685c00889a790ac
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 18:50:56 +0000

    hammering in

--- XoMaze/player/Player.py
+++ XoMaze/player/Player.py
@@ -12,22 +12,18 @@ class Player:
 		self.game = game
 		self.id = id
 		self.initVariables()
-		self.initVisuals()
 
 	def initVariables( self ):
 		self.colors = PLAYERCOLORS[ self.id ]
 		self.position = ( 0.0, 0.0 )
+		self.oldDirection = -1
+		self.isHeadAttached = False
 		self.directionToStringDirection = {
 			0 : "north",
 			1 : "east",
 			2 : "south",
 			3 : "west",
 		}
-
-	def initVisuals( self ):
-		'''
-		Have to get colors from somewhere...
-		'''
 
 	def move( self, direction ):
 		'''
@@ -39,17 +35,30 @@ class Player:
 		'''
 		print "Player %d is moving %s (%d)" % ( self.id, self.directionToStringDirection[ direction ], direction )
 		if direction == 0:
-			potentialPosition = ( self.position[0], self.position[1] + PLAYERYINCREMENT ) 
+			potentialPosition = [ self.position[0], self.position[1] + PLAYERYINCREMENT ]
+			if self.oldDirection == 1 or self.oldDirection == 3:
+				potentialPosition[0] = int( potentialPosition[0] ) + 0.5
 		elif direction == 1:
-			potentialPosition = ( self.position[0] + PLAYERXINCREMENT, self.position[1] ) 
+			potentialPosition = [ self.position[0] + PLAYERXINCREMENT, self.position[1] ] 
+			if self.oldDirection == 0 or self.oldDirection == 2:
+				potentialPosition[0] = int( potentialPosition[0] ) + 0.5
 		elif direction == 2:
-			potentialPosition = ( self.position[0], self.position[1] - PLAYERYINCREMENT ) 
+			potentialPosition = [ self.position[0], self.position[1] - PLAYERYINCREMENT ] 
+			if self.oldDirection == 1 or self.oldDirection == 3:
+				potentialPosition[0] = int( potentialPosition[0] ) + 0.5
 		else:
-			potentialPosition = ( self.position[0] - PLAYERXINCREMENT, self.position[1] ) 
-
+			potentialPosition = [ self.position[0] - PLAYERXINCREMENT, self.position[1] ] 
+			if self.oldDirection == 0 or self.oldDirection == 2:
+				potentialPosition[0] = int( potentialPosition[0] ) + 0.5
+		
+		self.oldDirection = direction
+		
+		# If my new discreet position is the same as my old one, update position
+		# and return, no need to check other walls
 		if self.getDiscreetPosition( potentialPosition ) == self.getDiscreetPosition( self.position ):
 			self.position = potentialPosition
 			return
+		
 		potentialCell = self.game.maze.getCell( *self.getDiscreetPosition( potentialPosition ) )
 		directionObject = getattr( potentialCell, self.directionToStringDirection[ direction ] )
 		if directionObject.isWalled:
@@ -64,6 +73,8 @@ class Player:
 		'''
 		Resets the player to the bottom of the maze, offset based on his id
 		'''
+		self.oldDirection = -1
+		self.isHeadAttached = False
 		x = self.game.maze.getColumnCount() / 2.0
 		self.position = ( x - float(self.game.numberOfPlayers) + self.id*1.0, 0.0 )
 
@@ -74,11 +85,14 @@ class Player:
 		return ( int( position[0] ), int( position[1] ) )
 
 	def getStrokeColor( self ):
-		return self.colors[ 0 ]
+		return self.colors[0]
+
 	def getFillColor( self ):
-		return self.colors[ 1 ]
+		return self.colors[1]
+
 	def signal( self ):
 		'''
 		Wave and change strong path to where you are
 		'''
 		pass
+
