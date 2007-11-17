588de29b6b10ab19d85bfa1863fda53b042f2900
shaskell <shaskell@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 08:27:33 +0000

    quit!

--- trunk/XoMaze/XoMaze.py
+++ trunk/XoMaze/XoMaze.py
@@ -1,10 +1,14 @@
 import pygame
+import sys
+#from olpcgames import eventwrap
 from Maze import Maze, Cell
 
 class XoMaze:
     def __init__(self, width=1200,height=825):
         self.initScreen( width, height ) 
         self.maze = Maze()
+        pygame.init()
+        #eventwrap.install()
         
     def initScreen( self, width, height ):
         """Set the window Size"""
@@ -21,3 +25,7 @@ class XoMaze:
     	'''
     	pygame.display.update()
     	self.maze.paint( self.screen, 0, 0, 1000, 700 )
+    	for event in pygame.event.get():
+			if event.type == pygame.QUIT: 
+				sys.exit()
+        
