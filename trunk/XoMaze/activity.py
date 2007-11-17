197e2f57652d55343e43b8f7ec28e03a4e06c9a7
shaskell <shaskell@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 09:59:03 +0000

    NOW runs on the xo

--- trunk/XoMaze/activity.py
+++ trunk/XoMaze/activity.py
@@ -3,6 +3,6 @@ import olpcgames
 # Class name must match 'class' property in activity/activity.info:
 class XoMazeActivity(olpcgames.PyGameActivity):
 	"""A Pygame game as a Sugar activity."""
-	game_name = 'XoMaze'        # game_name must match name of your Pygame module
+	game_name = 'runXoMaze'        # game_name must match name of your Pygame module
 	game_title = 'XoMaze'
 	game_size = (1200,825)
