3ff4f0127c6965e2184f93bcb5503cc94e8b3e99
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 20:10:19 +0000

    keys moved to globals

--- XoMaze/XoMaze.py
+++ XoMaze/XoMaze.py
@@ -59,24 +59,10 @@ class XoMaze:
 		
 		if emulatorMode:
 			# These are the directional keys on the laptop's joystick
-			self.keysToDirections = {
-				"K_KP8" : [ 0, 0, ],
-				"K_KP6" : [ 0, 1, ],
-				"K_KP2" : [ 0, 2, ],
-				"K_KP4" : [ 0, 3, ],
-			}
+			self.keysToDirections = EMULATORKEYS
 		else:
 			# These are... wasd
-			self.keysToDirections = {
-				"K_w" : [ 0, 0, ],
-				"K_d" : [ 0, 1, ],
-				"K_s" : [ 0, 2, ],
-				"K_a" : [ 0, 3, ],
-				"K_UP" : [ 1, 0, ],
-				"K_RIGHT" : [ 1, 1, ],
-				"K_DOWN" : [ 1, 2, ],
-				"K_LEFT" : [ 1, 3, ],
-			}
+			self.keysToDirections = KEYBOARDKEYS
 
 	def initHud( self ):
 		pass
