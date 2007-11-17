64d86d1ae7b6cd5e2b919fc5fd16a0c7f2ac2473
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 12:40:55 +0000

    wanted to delete... couldn't figure out how, this is next best thing...

--- trunk/runXoMaze.py
+++ trunk/runXoMaze.py
@@ -1,15 +1,1 @@
-import pygame
-#import olpcgames ##note, disable this when testing on a non-xo computer.
-from XoMaze import XoMaze
-
-def main():
-    pygame.init()
-    # Start Pygame displays...
-    game = XoMaze( 1200, 855 )
-    while True:
-        # Pygame event loop.
-        # Update the screen
-		game.update()
-		
-if __name__=="__main__":
-    main()
+Not the right file!
