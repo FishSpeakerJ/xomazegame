979bf39821220e52365cababbf9ca35b035bf87a
shaskell <shaskell@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 06:15:07 +0000

    zimple maze loop working

--- XoMaze.py
+++ XoMaze.py
@@ -1,5 +1,6 @@
 import pygame
 from Maze import Maze, Cell
+
 class XoMaze:
     def __init__(self, width=1200,height=825):
         self.initScreen( width, height ) 
@@ -12,3 +13,7 @@ class XoMaze:
         """Create the Screen"""
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('XoMaze')
+    
+    def update( self ):
+    	pygame.display.update()
+    	self.maze.paint( self.screen, 0, 0, 1000, 700 )
