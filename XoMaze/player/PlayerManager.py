5923fd0860398f736fdf04d340d7543e50bafbae
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-17 22:39:24 +0000

    finished is working

--- XoMaze/player/PlayerManager.py
+++ XoMaze/player/PlayerManager.py
@@ -15,13 +15,17 @@ class PlayerManager:
 			self.playerIdsToPlayers[ i ] = Player( self.game, i )
 	
 	def reset( self ):
+		self.playersWhoHaveHeads = []
 		for player in self.playerIdsToPlayers.values():
 			player.reset()
 
 	def foundHead( self, id ):
 		print "Player %d found their head!" % id
+		self.playersWhoHaveHeads.append( self.playerIdsToPlayers[ id ] )
 	
 	def finished( self, id ):
 		print "Player %d finished!!" % id
-
-		
+		for player in self.playerIdsToPlayers.values():
+			if not player.isFinished():
+				return
+		self.game.gameOver()
