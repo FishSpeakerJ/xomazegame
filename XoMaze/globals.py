ad7adda93694dcd00788dbdd82e35674cdb9407d
shawnpatton <shawnpatton@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-18 11:07:41 +0000

    signal controls removed

--- XoMaze/globals.py
+++ XoMaze/globals.py
@@ -26,27 +26,21 @@ emulatorKeys = {
 	pygame.K_KP6 : [ 0, 1, ],
 	pygame.K_KP2 : [ 0, 2, ],
 	pygame.K_KP4 : [ 0, 3, ],
-	pygame.K_TAB : [ 0, 4, ],
 
 	pygame.K_w : [ 1, 0, ],
 	pygame.K_d : [ 1, 1, ],
 	pygame.K_s : [ 1, 2, ],
 	pygame.K_a : [ 1, 3, ],
-	pygame.K_q : [ 1, 4, ],
 	
 	pygame.K_UP : [ 2, 0, ],
 	pygame.K_RIGHT : [ 2, 1, ],
 	pygame.K_DOWN : [ 2, 2, ],
 	pygame.K_LEFT : [ 2, 3, ],
-	pygame.K_RSHIFT : [ 2, 4, ],
 	
 	pygame.K_KP9 : [ 3, 0, ],
 	pygame.K_KP1 : [ 3, 1, ],
 	pygame.K_KP3 : [ 3, 2, ],
-	pygame.K_KP7 : [ 3, 3, ],
-	pygame.K_BACKSPACE : [ 3, 4, ],
-	
-	
+	pygame.K_KP7 : [ 3, 3, ]
 }
 
 keyboardKeys = {
@@ -54,26 +48,22 @@ keyboardKeys = {
 	pygame.K_d : [ 0, 1, ],
 	pygame.K_s : [ 0, 2, ],
 	pygame.K_a : [ 0, 3, ],
-	pygame.K_q : [ 0, 4, ],
 	
 	pygame.K_g : [ 1, 0, ], # up
 	pygame.K_h : [ 1, 0, ], # up
 	pygame.K_n : [ 1, 1, ],
 	pygame.K_b : [ 1, 2, ],
 	pygame.K_v : [ 1, 3, ],
-	pygame.K_f : [ 1, 4, ],
 	
 	pygame.K_UP : [ 2, 0, ],
 	pygame.K_RIGHT : [ 2, 1, ],
 	pygame.K_DOWN : [ 2, 2, ],
 	pygame.K_LEFT : [ 2, 3, ],
-	pygame.K_RSHIFT : [ 2, 4, ],
 
 	pygame.K_KP8 : [ 3, 0, ],
 	pygame.K_KP6 : [ 3, 1, ],
 	pygame.K_KP2 : [ 3, 2, ],
 	pygame.K_KP4 : [ 3, 3, ],
-	pygame.K_KP7 : [ 3, 4, ],
 }
 # Player variables
 playerSpeedConstant = 0.21  # playerSpeed = sqrt( totalCellsInMaze )*playerSpeedConstant
