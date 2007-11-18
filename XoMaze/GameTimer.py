83355fbe833d81d294e6c4e7aedaf12ca74d5cf7
jfpratt <jfpratt@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-18 15:51:44 +0000

    fixed clock issues

--- XoMaze/GameTimer.py
+++ XoMaze/GameTimer.py
@@ -7,18 +7,32 @@ class GameTimer:
 	def __init__( self ):
 		# start game time
 		self.startTime = 0
+		self.stoppedTime = 0
 		self.isRunning = False
 
 	def start( self ):
 		self.startTime = pygame.time.get_ticks()
+		self.stoppedTime = self.startTime
 		self.isRunning = True
-		
+
 	def stop( self ):
-		self.isRunning = False
+		if self.isRunning:
+			self.stoppedTime = pygame.time.get_ticks()
+			self.isRunning = False
+
+	def reset( self ):
+		self.startTime = 0
+		self.stoppedTime = 0
 
 	def getTime( self ):
-		return pygame.time.get_ticks() - self.startTime
-		
+		if self.isRunning:
+			return pygame.time.get_ticks() - self.startTime
+		else:
+			return self.stoppedTime - self.startTime
+
 	def getTimeString( self ):
-		delta = datetime.timedelta( milliseconds=(pygame.time.get_ticks() - self.startTime) )
-		return time.strftime("%M:%S",time.gmtime(delta.seconds))
+		if self.isRunning:
+			delta = datetime.timedelta( milliseconds=(pygame.time.get_ticks() - self.startTime) )
+		else:
+			delta = datetime.timedelta( milliseconds=(self.stoppedTime - self.startTime) )
+		return time.strftime( "%M:%S", time.gmtime( delta.seconds ) )
