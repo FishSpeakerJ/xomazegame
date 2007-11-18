51f95beb9b2abe9c061254acec9d2191b9630b66
shaskell <shaskell@d631444b-123f-0410-a5fc-6d59da041ff2>
2007-11-18 15:24:29 +0000

    don't show timer at start

--- trunk/XoMaze/Hud.py
+++ trunk/XoMaze/Hud.py
@@ -9,21 +9,20 @@ class Hud:
 
 	def update( self ):
 		# Render the text
-		if self.game.gameClock.isRunning:
+		if self.game.isGameRunning:
 			text = self.font.render(self.game.gameClock.getTimeString(), True, 
 			                        (255, 255, 255), (0, 0, 0))
+			# Create a rectangle
+			textRect = text.get_rect()
+	
+			# Center the rectangle
+			textRect.centerx = self.surface.get_rect().centerx
+			textRect.centery = self.surface.get_rect().centery
+	
+			# Blit the text
+			self.surface.blit(text, textRect)
+			pygame.display.update( self.surface.get_rect() )
+	#		self.game.addDirtyRect( self.surface.get_rect() )
+	
 		else:
-			text = self.font.render("00:00", True, 
-			                        (255, 255, 255), (0, 0, 0))
-
-		# Create a rectangle
-		textRect = text.get_rect()
-
-		# Center the rectangle
-		textRect.centerx = self.surface.get_rect().centerx
-		textRect.centery = self.surface.get_rect().centery
-
-		# Blit the text
-		self.surface.blit(text, textRect)
-		pygame.display.update( self.surface.get_rect() )
-#		self.game.addDirtyRect( self.surface.get_rect() )
+			pass
